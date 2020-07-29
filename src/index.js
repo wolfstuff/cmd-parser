43539c640ae9e0c78f583d6a997b7e0d2dfde6c8
Emmanuelle Lacoeur <ficta.voluptatis@gmail.com>
2020-07-29 09:29:46 -0700

    src/

---
+++ src/index.js
@@ -0,0 +1,147 @@
+'use strict';
+
+const parser = require('./parser');
+
+/**
+ * A Discord message object.
+ *
+ * @typedef {object} Message
+ * @property {string}   content
+ * @property {Function} delete
+ */
+
+/**
+ * A Function that executes a particular command.
+ *
+ * @typedef {Function} CommandFunction
+ * @param   {Message} message - The Discord message that triggered the command.
+ * @param   {...*}    args    - Any other arguments.
+ */
+
+/**
+ * A key-value map of commands to use by Discord bot clients. Each key must be a string,
+ * representing the name of the command, and each value must be a Function that executes
+ * each command.
+ *
+ * @typedef {object.<string, CommandFunction>} CommandsObject
+ */
+
+/**
+ * Default options used to determine the behavior of the command parser.
+ *
+ * @type {object}
+ * @property {string}   commandPrefix         - The single-character prefix used to recognize commands.
+ * @property {Function} fnError               - A function called when command execution fails.
+ * @property {Function} fnRemoveMessage       - A function optionally called to remove a command message.
+ * @property {Function} fnUnrecognized        - A function called when an unrecognized command is parsed.
+ * @property {boolean}  removeCommandMessages - If `true`, calls `fnRemoveMessage` for each command message.
+ * @private
+ */
+const defaultOpts = {
+    commandPrefix:         '!',
+    fnError:               errorHandler,
+    fnRemoveMessage:       removeMessage,
+    fnUnrecognized:        unrecognizedHandler,
+    removeCommandMessages: true
+};
+
+/**
+ * Creates and initializes a command parser for a Discord bot that will respond to a certain
+ * set of {commands}. Commands are recognized based on an optional prefix, which defaults to
+ * `!`. By default, when a Discord user sends a bot command, the bot will automatically remove
+ * the message that triggered the command; this behavior can be changed if an optional flag
+ * is set to `false` instead of its default `true` value.
+ *
+ * @param   {CommandsObject} commands - Commands to use by the bot client.
+ * @param   {object}         opts     - The prefix used by all commands; the `!` in `!command`.
+ * @returns {Function}                - A handler function that parses messages for commands.
+ * @public
+ * @example
+ *     const client = new Client(); // Discord example
+ *     client.on('message', makeParser(commands, { prefix: '#' }));
+ *     client.login('YOUR_DISCORD_BOT_TOKEN_HERE');
+ */
+module.exports = function makeParser(commands, opts) {
+    const options = Object.assign(Object.create(null), defaultOpts, opts);
+    const parse   = parser(options.commandPrefix);
+
+    /**
+     * Processes messages, parsing their contents for commands and arguments and dispatching
+     * to the appropriate command function.
+     *
+     * @param   {Message} message - The message that triggered the command.
+     * @returns {Promise}         - When the message has been parsed and the command has been executed.
+     * @public
+     * @example
+     *     const client = new Client(); // Discord example
+     *     const messageHandler = makeParser(commands, { prefix: '#' });
+     *     client.on('message', messageHandler);
+     *     client.login('YOUR_DISCORD_BOT_TOKEN_HERE');
+     */
+    return async function messageHandler(message) {
+        const parsed = parse(message.content);
+
+        if (!parsed.command) {
+            // This message is not a command; so ignore the input:
+            return;
+        } else if (Object.prototype.hasOwnProperty.call(commands, parsed.command)) {
+            try {
+                if (options.removeCommandMessages) {
+                    await options.fnRemoveMessage(message);
+                }
+
+                return commands[parsed.command].call(null, message, ...parsed.args);
+            } catch (e) {
+                return options.fnError(message, e);
+            }
+        }
+
+        return options.fnUnrecognized(message);
+    };
+};
+
+/**
+ * An error handling function called whenever command execution triggers an error.
+ *
+ * @param   {Message} message - The Discord message with the command that triggered an error.
+ * @param   {Error}   error   - The error that was triggered.
+ * @returns {Promise}         - When the funcion is finished reporting an error.
+ * @private
+ * @example
+ *     errorHandler(message, new Error('This message broke something!'));
+ */
+function errorHandler(message, error) {
+    const { id }               = message.author;
+    const { channel, content } = message;
+
+    return channel.send(`<@${ id }>, \`${ content }\` resulted in \`${ error.message }\`.`);
+}
+
+/**
+ * Removes a given {message} from chat history.
+ *
+ * @param   {Message} message - The message to be removed.
+ * @returns {Promise}         - When the message has been removed.
+ * @private
+ * @example
+ *     removeMessage(message);
+ */
+function removeMessage(message) {
+    return message.delete();
+}
+
+/**
+ * A function called whenever the parser encounters an unrecognized command.
+ *
+ * @param   {Message} message - The message that triggered the command.
+ * @returns {Promise}         - When the funcion is finished reporting an unrecognized command.
+ * @private
+ * @example
+ *     unrecognizedHandler(message);
+ */
+function unrecognizedHandler(message) {
+    const { id }               = message.author;
+    const { channel, content } = message;
+
+    return channel.send(`<@${ id }>, I don't recognize the command \`${ content }\`.`);
+}
---
+++ src/parser.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * The Regular Expression used to separate arguments from a given string of text.
+ *
+ * @type {RegExp}
+ * @private
+ */
+const args_expr = /(?<single>[^\s"]+)|"(?<multi>[^"]*)"/gu;
+
+/**
+ * Returns a function used to parse commands via regular expression. Arguments are returned in
+ * positional order; named arguments are not currently supported.
+ *
+ * @param   {string} prefix - The single-character prefix used to determine commands.
+ * @returns {Function}      - A command parsing function.
+ * @public
+ * @example
+ *     const parse = parser('#'); // parses commands that begin with `#`
+ */
+module.exports = function parser(prefix) {
+    /**
+     * The Regular Expression used to match and split a command and its arguments, if any.
+     *
+     * @type {RegExp}
+     * @private
+     */
+    const cmd_expr = new RegExp(`(?:^${ prefix })(?<command>[\\w]+)(?: +?)?(?<args>.+$)?`, 'gu');
+
+    /**
+     * An object returned as a result of successfully parsing a string as a command.
+     *
+     * @typedef  {object} CommandObject
+     * @property {string}         command - The command parsed.
+     * @property {Array.<string>} args    - Any arguments provided.
+     */
+
+    /**
+     * Parses a given string {str} as a command. If the string is not a command, returns the string;
+     * otherwise, returns an object representing the command and any arguments passed to it.
+     *
+     * @param   {string}           str - The string to parse.
+     * @returns {string|CommandObject} - The string, or a CommandObject.
+     * @public
+     * @example
+     *     const parse  = parser('!');
+     *     const result1 = parse('!do the thing');
+     *     const result2 = parse('!hey "double quotes" are pretty neat');
+     *     const result3 = parse('!empty');
+     *     const result4 = parse('Hello, world!'); // 'Hello, world!'
+     *     result1.command; // 'do'
+     *     result1.args; // [ 'the', 'thing' ]
+     *     result2.command; // 'hey'
+     *     result2.args; // [ 'double quotes', 'are', 'pretty', 'neat' ]
+     *     result3.command; // 'empty'
+     *     result3.args; // []
+     */
+    return function parse(str) {
+        if (str[0] !== prefix.charAt(0)) {
+            return str;
+        }
+
+        const matched = Object.assign(Object.create(null), cmd_expr.exec(str).groups);
+        const command = matched.command;
+        const args    = (matched.args !== undefined)
+            ? matched.args.match(args_expr).map((arg) => {
+                return arg.split('"')
+                    .join('')
+                    .trim();
+            })
+            : [];
+
+        return { command, args };
+    };
+};
